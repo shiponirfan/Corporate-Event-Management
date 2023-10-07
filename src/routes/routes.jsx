d4c2361ac53a7a2b05f0d81b419b230fb5df48d2
shiponirfan <imshiponirfan@gmail.com>
2023-10-07 15:29:22 +0600

    Corporate Event Management Website

---
+++ src/routes/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>
+    },
+])
+export default routes;
